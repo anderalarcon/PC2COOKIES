4717b29d6a8bddab534d2982cd5acacffcbbece6
Roger_Vega_Solis <raavs0318@gmail.com>
2021-10-03 00:57:00 -0500

    pregunta 2 listo visualmente

--- app/src/main/java/pe/edu/ulima/pm/cookies/fragments/RecetasFragment.kt
+++ app/src/main/java/pe/edu/ulima/pm/cookies/fragments/RecetasFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import pe.edu.ulima.pm.cookies.Adapter.RecetaListAdapter
 import pe.edu.ulima.pm.cookies.R
+import pe.edu.ulima.pm.cookies.models.RecetasManager
 
 class RecetasFragment: Fragment() {
     override fun onCreateView(
@@ -15,5 +18,12 @@ class RecetasFragment: Fragment() {
     ): View? {
         //activity.metodoCualquiera()
         return inflater.inflate(R.layout.fragment_recetas,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rviProducts = view.findViewById<RecyclerView>(R.id.rviRecetas)
+        rviProducts.adapter = RecetaListAdapter(RecetasManager().getRecetas())
     }
 }
